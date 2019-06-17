bbf94ceba05df8164448626ced29e78106835fa6
Evrin Lumbantobing <evrintobing@gmail.com>
2019-06-17 16:13:39 +0700

    Add face recognition func

--- app.py
+++ app.py
@@ -4,13 +4,14 @@ from flask import Flask, json, Response, request
 from werkzeug.utils import secure_filename
 from os import path, getcwd
 from db import Database
-
+from face import Face
 
 app = Flask(__name__)
 
 app.config['file_allowed'] = ['image/png','image/jpeg']
 app.config['saves'] = path.join(getcwd(), 'saves')
 app.db = Database()
+app.face = Face(app)
 
 
 def success_handle(output, status=200, mimetype='application/json'):
@@ -19,6 +20,41 @@ def success_handle(output, status=200, mimetype='application/json'):
 
 def error_handle(error_message, status=500, mimetype='application/json'):
     return Response(json.dumps({"error": {"message": error_message}}), status=status, mimetype=mimetype)
+
+
+def get_user_by_id(user_id):
+    user = {}
+
+    results = app.db.select('SELECT users.id, users.name, users.created, faces.id, faces.user_id, faces.filename, '
+                            'faces.created FROM users LEFT JOIN faces ON faces.user_id WHERE users.id = ?', [user_id])
+    index = 0
+    for row in results:
+        print(row)
+        face = {
+            "id": row[3],
+            "user_id": row[4],
+            "filename": row[5],
+            "created": row[6]
+        }
+        if index == 0:
+            user ={
+                "id": row[0],
+                "name": row[1],
+                "created": row[2],
+                "faces": []
+            }
+        if 3 in row:
+
+            user["faces"].append(face)
+        index = index + 1
+    if 'id' in user:
+        return user
+    return None
+
+
+def delete_user_by_id(user_id):
+    app.db.delete('DELETE FROM users WHERE users.id = ?', [user_id])
+    app.db.delete('DELETE FROM faces WHERE faces.user_id = ?', [user_id])
 
 
 @app.route('/', methods=['GET'])
@@ -58,8 +94,8 @@ def train():
             print("file allowed and save in", app.config['saves'])
 
             filename = secure_filename(file.filename)
-
-            file.save(path.join(app.config['saves'], filename))
+            known_saves = path.join(app.config['saves'], 'known')
+            file.save(path.join(known_saves, filename))
 
             print("new filename is", filename)
 
@@ -90,8 +126,48 @@ def train():
         print("Success")
     return success_handle(output)
 
-# @app.route('/api/users/<int:user)id>')
-# def user_profile()
+
+@app.route('/api/users/<int:user_id>', methods=['GET', 'DELETE'])
+def user_profile(user_id):
+
+    if request.method == 'GET':
+        user = get_user_by_id(user_id)
+
+        if user:
+            return success_handle(json.dumps(user), 200)
+        else:
+            return error_handle("User Not Found")
+    if request.method == 'DELETE':
+
+        delete_user_by_id(user_id)
+        return success_handle(json.dumps({"deleted": True}))
+
+
+@app.route('/api/recognize', methods=['POST'])
+def recognition():
+
+    if 'file' not in request.files:
+        return error_handle("image require")
+    else:
+        file = request.files['file']
+        if file.mimetype not in app.config['file_allowed']:
+            return error_handle("File not Allowed")
+        else:
+            filename = secure_filename(file.filename)
+            unknown_saves = path.join(app.config['saves'], 'unknown')
+            file_path = path.join(unknown_saves, filename)
+            file.save(file_path)
+
+            user_id = app.face.recognize(filename)
+            if user_id:
+                user = get_user_by_id(user_id)
+                message = {"message": "Found {0}".format(user['name']), "user": user}
+                return success_handle(json.dumps(message))
+            else:
+                return error_handle("Sorry Can't Found any people match, try again")
+
+    return success_handle(json.dumps({"filename_to_compare_is": filename}))
+
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5003)
+    app.run()
