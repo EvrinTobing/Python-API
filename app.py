0ca16c355f5e6b08e7ddcde2fca1d7868bef2323
Evrin Tobing <evrintobing@gmail.com>
2019-09-24 19:06:43 +0700

    Update

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 import datetime
 
 from flask_socketio import SocketIO
-from flask import Flask, json, Response, request, jsonify
+from flask import Flask, json, Response, request, jsonify, send_file
 from werkzeug.utils import secure_filename
 from os import path, getcwd
 from db import Database
 from face import Face
+from Aru_py import Aruco
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'asdewq123'
@@ -15,6 +16,7 @@ app.config['saves'] = path.join(getcwd(), 'saves')
 app.config['static'] = path.join(getcwd(), 'static')
 app.db = Database()
 app.face = Face(app)
+app.Aru_py = Aruco(app)
 
 date = datetime.datetime.now().strftime("%m_%d_%Y_%H_%M_%S")
 
@@ -107,6 +109,16 @@ def homepage():
     output = json.dumps({"api": '1.0'})
     return Response(output, status=200, mimetype='application/json')
 
+@app.route('/loc', methods=['POST'])
+def loc():
+    file = request.files['file']
+    file_allowed()
+    lokasi = app.face.face_locate(file)
+    print("ok")
+    result = json.dumps(lokasi)
+    return result
+
+
 
 @app. route('/fav', methods=['GET'])
 def get_favourites():
@@ -127,12 +139,12 @@ def get_recommendation_for_user(user_id = None):
     results = []
 
     for d in data:
-        results.append({"id": d[0], "name": d[1], "description": d[2], "price": d[3], "images": d[4]})
+        rec = results.append({"id": d[0], "name": d[1], "description": d[2], "price": d[3], "images": d[4]})
 
     if len(results) == 0:
         return get_favourites()
     else:
-        resp = json.dumps(results)
+        resp = json.dumps(rec)
         print(resp)
         return resp
 
@@ -155,7 +167,7 @@ def insert_product():
     return json.dumps("ok")
 
 
-@app.route('/api/recommendation', methods=['POST'])
+@app.route('/Face_rec', methods=['POST' ,'GET'])
 def recommendation():
     file_allowed()
     file = request.files['file']
@@ -164,14 +176,22 @@ def recommendation():
     user = get_user_by_id(user_id)
 
     if user:
-        print("Existing customer")
+        print("Existing customer; id = {}".format(user_id) )
+        # images()
         return get_recommendation_for_user(user_id)
     else:
         print("New customer")
         save_new_face(file)
-
         return get_favourites()
 
+def images():
+    file = request.files['file']
+    user_id = app.face.recognize(file)
+    file_name = str(user_id)
+    file_names = secure_filename(file_name+ ".jpg")
+    known_saves = path.join(app.config['saves'], 'known')
+    file_path = path.join(known_saves, file_names)
+    return send_file(file_path, mimetype='image/jpeg')
 
 @app.route('/api/order',methods=['POST'])
 def order():
@@ -186,6 +206,14 @@ def order_data(data):
                            'id_corp, id_tenant, id_device, created) VALUES (?,?,?,?,?,?,?,?)',
                            (data['catalog_id'], data['user_id'], data['quantity'], data['total_amount'], data['corp_id'], data['tenant_id'], data['device_id'], created))
     return success_handle(json.dumps("ok"))
+
+
+@app.route('/aruco', methods=['POST'])
+def known():
+    file_allowed()
+    file = request.files['file']
+
+    mark = app.Aru_py.marker(file)
 
 
 if __name__ == '__main__':
--- face.py
+++ face.py
@@ -1,10 +1,11 @@
 import face_recognition
 from os import path
 import numpy as np
+from PIL import Image
+import cv2
 
 faces_to_compare = []
 filenames_to_compare = []
-
 
 
 class Face:
@@ -66,11 +67,6 @@ class Face:
         print(minIdx)
 
         minValue = np.min(match_results)
-
-        # for i in range(len(match_results)):
-        #     if match_results[i]:
-        #         name_found = filenames_to_compare[i]
-        #         print(name_found + " " + str(match_results[i]))
         print(minValue)
 
         if minValue > 0.5:
@@ -84,3 +80,75 @@ class Face:
         unknown_encoding_images = face_recognition.face_encodings(unknown_image)[0]
         self.known_encoding_faces.append(unknown_encoding_images)
 
+    # def video(self, file_stream):
+    #     file_stream.seek(0)
+    #     file_stream = cv2.VideoCapture()
+    #     length = int(file_stream.get(cv2.CAP_PROP_FRAME_COUNT))
+    #     unknown_image = face_recognition.load_image_file(file_stream)
+    #     unknown_encoding_images = face_recognition.face_encodings(unknown_image)[0]
+    #
+    #     face_locations = []
+    #     face_encodings = []
+    #     face_names = []
+    #     frame_number = 0
+    #
+    #     while True:
+    #         # Grab a single frame of video
+    #         ret, frame = file_stream.read()
+    #         frame_number += 1
+    #
+    #         # Quit when the input video file ends
+    #         if not ret:
+    #             break
+    #
+    #         # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
+    #         rgb_frame = frame[:, :, ::-1]
+    #
+    #         # Find all the faces and face encodings in the current frame of video
+    #         face_locations = face_recognition.face_locations(rgb_frame)
+    #         face_encodings = face_recognition.face_encodings(rgb_frame, face_locations)
+    #
+    #         face_names = []
+    #         for face_encoding in face_encodings:
+    #             # See if the face is a match for the known face(s)
+    #             match = face_recognition.compare_faces(known_faces, face_encoding, tolerance=0.50)
+    #
+    #             # If you had more than 2 faces, you could make this logic a lot prettier
+    #             # but I kept it simple for the demo
+    #             name = None
+    #             if match[0]:
+    #                 name = "Lin-Manuel Miranda"
+    #             elif match[1]:
+    #                 name = "Alex Lacamoire"
+    #
+    #             face_names.append(name)
+    #
+    #         # Label the results
+    #         for (top, right, bottom, left), name in zip(face_locations, face_names):
+    #             if not name:
+    #                 continue
+    #
+    #             # Draw a box around the face
+    #             cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+    #
+    #             # Draw a label with a name below the face
+    #             cv2.rectangle(frame, (left, bottom - 25), (right, bottom), (0, 0, 255), cv2.FILLED)
+    #             font = cv2.FONT_HERSHEY_DUPLEX
+    #             cv2.putText(frame, name, (left + 6, bottom - 6), font, 0.5, (255, 255, 255), 1)
+    #
+    #         # Write the resulting image to the output video file
+    #         print("Writing frame {} / {}".format(frame_number, length))
+    #         output_movie.write(frame)
+
+
+    def face_locate(self, file_stream):
+        file_stream.seek(0)
+        image = face_recognition.load_image_file(file_stream)
+        face_locations = face_recognition.face_locations(image)
+        print("I found {} face(s) in this photograph.".format(len(face_locations)))
+        for face_location in face_locations:
+            top, right, bottom, left = face_location
+            print("A face is located at pixel location Top: {}, Left: {}, Bottom: {}, Right: {}".format(top, left, bottom, right))
+
+            face_image = image[top:bottom, left:right]
+            pil_image = Image.fromarray(face_image)
